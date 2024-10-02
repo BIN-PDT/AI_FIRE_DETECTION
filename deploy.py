495d04aeeb9fe19b3208d93f8f3d00768b66ce71
BIN BIN <thangpd300502@gmail.com>
2024-10-02 18:19:16 +0700

    CHANGE MODEL & DEPLOY MODEL SCRIPT

---
+++ deploy.py
@@ -0,0 +1,15 @@
+from os import environ
+from roboflow import Roboflow
+from dotenv import load_dotenv
+
+
+load_dotenv()
+API_KEY = environ.get("API_KEY")
+WORKSPACE = environ.get("WORKSPACE")
+PROJECT = environ.get("PROJECT")
+VERSION = environ.get("VERSION")
+
+rf = Roboflow(api_key=API_KEY)
+project = rf.workspace(WORKSPACE).project(PROJECT)
+version = project.version(VERSION)
+version.deploy("yolov10", "model", "model.pt")
